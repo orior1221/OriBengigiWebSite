c2a576e4b0424671438a420dcbd0af2e518f68c5
orior1221 <orior2204@gmail.com>
2026-04-26 14:08:26 +0300

    stone free

--- OriBengigiWebSite/Login.aspx.cs
+++ OriBengigiWebSite/Login.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,18 +12,45 @@ public partial class Login : System.Web.UI.Page
     {
         if (IsPostBack)
         {
-            if (Request.Form["userName"] == "test" && Request.Form["password"] == "1234")
+            int result = GetUserTypeFromDB(Request.Form["username"], Request.Form["password"]);
+            
+            if (result > 0)
             {
-                Session["userName"] = Request.Form["userName"];
+                Session["userName"] = Request.Form["username"];
                 Session["isLoggedIn"] = true;
 
                 Response.Redirect("Default.aspx");
             }
             else
             {
-                LoginResult.InnerText = "שם משתמש או סיסמה אינם נכונים";
+                LoginResult.InnerText = "Password or username incorrect";
             }
         }
-
     }
+
+
+    //returns:
+    //0 - if user is not valid
+    //1 - is user is valid
+    private int GetUserTypeFromDB(string userName, string password)
+    {
+        string dbPath = this.MapPath("App_Data/Database.mdf");
+        DAL dal = new DAL(dbPath);
+
+        string sqlQuery = "SELECT * FROM Users " +
+                            "WHERE user_name = '" + userName +
+                            "' AND pswd = '" + password + "'";
+
+        DataTable dt = dal.GetDataTable(sqlQuery);
+
+        if (dt.Rows.Count == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
 }
