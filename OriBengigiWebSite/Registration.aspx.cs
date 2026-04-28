eebb63a361914e86e19ad1d539c0f29afdc436ad
orior1221 <orior2204@gmail.com>
2026-04-28 09:10:54 +0300

    heaven's door

--- OriBengigiWebSite/Registration.aspx.cs
+++ OriBengigiWebSite/Registration.aspx.cs
@@ -70,15 +70,6 @@ public partial class Registration : System.Web.UI.Page
             return false;
         }
 
-        // === משימה לתלמיד: וידוא שם משתמש ===
-        // 1. בדוק אם אורך שם המשתמש קטן מ-3 או גדול מ-8 תווים
-        // לדוגמה של בדיקת אורך, הסתכל בפעולה:
-        // First_Name_Validation
-        // 2. אם האורך לא תקין, הוסף הודעת שגיאה לתוך:
-        // RegistrationResult.InnerText
-        // 3. ולאחר מכן עצור את הפעולה על ידי:
-        // return false;
-
         return true;
     }
 
@@ -125,35 +116,55 @@ public partial class Registration : System.Web.UI.Page
 
     private bool ID_Validation()
     {
-        // === משימה לתלמיד: וידוא תעודת זהות ===
-        // 1. ודא שאורך תעודת הזהות הוא בדיוק 9 תווים
-        // 2. ודא שכל התווים במחרוזת הם ספרות בלבד
-        // כדי לעבור על כל התווים, תוכל להיעזר בלולאה שמופיעה בפעולה:
-        // Password_Validation
-        // 3. אם יש שגיאה, אל תשכח להוסיף הודעה אל:
-        // RegistrationResult.InnerText
-        // ולהחזיר:
-        // return false;
+        string id = idNum.Value;
 
+        if (id.Length != 9)
+        {
+            RegistrationResult.InnerText += "תעודת זהות חייבת להכיל בדיוק 9 ספרות. ";
+            return false;
+        }
+
+        for (int i = 0; i < id.Length; i++)
+        {
+            if (id[i] < '0' || id[i] > '9')
+            {
+                RegistrationResult.InnerText += "תעודת זהות חייבת להכיל ספרות בלבד. ";
+                return false;
+            }
+        }
         return true;
     }
 
+
     private bool Phone_Validation()
     {
-        // === משימה לתלמיד: וידוא מספר טלפון ===
-        // 1. ודא שאורך מספר הטלפון הוא בדיוק 10 תווים
-        // 2. ודא שהתו הראשון במספר הוא הספרה אפס
-        // 3. ודא שכל התווים במחרוזת הם ספרות בלבד
-        // 4. במקרה שאחד מהתנאים לא מתקיים, עדכן את:
-        // RegistrationResult.InnerText
-        // וסיים את הפעולה עם:
-        // return false;
+        string num = phone.Value; 
+        if (num.Length != 10)
+        {
+            RegistrationResult.InnerText += "מספר טלפון חייב להכיל בדיוק 10 ספרות. ";
+            return false;
+        }
 
+        if (num[0] != '0')
+        {
+            RegistrationResult.InnerText += "מספר טלפון חייב להתחיל בספרה 0. ";
+            return false;
+        }
+
+        for (int i = 0; i < num.Length; i++)
+        {
+            if (num[i] < '0' || num[i] > '9')
+            {
+                RegistrationResult.InnerText += "מספר טלפון חייב להכיל ספרות בלבד. ";
+                return false;
+            }
+        }
         return true;
     }
 
     private bool Email_Validation()
     {
+
         // === משימה לתלמיד: וידוא כתובת אימייל בסיסית ===
         // ודא שהתנאים הבאים מתקיימים:
         // 1. המחרוזת מכילה את התו שטרודל
@@ -163,7 +174,7 @@ public partial class Registration : System.Web.UI.Page
         // IndexOf
         // במקרה שאחד התנאים לא מתקיים, הוסף הודעת שגיאה מתאימה והחזר:
         // return false;
-
+        
         return true;
     }
 
